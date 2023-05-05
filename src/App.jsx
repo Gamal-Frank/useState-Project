8a44a460b7bfb308f824f9a8c63d30072a73db79
frank <gamalgamer10@yahoo.com>
2023-05-05 16:26:25 +0300

    multiple counters

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
 import { useState } from "react";
 
-function App() {
+const Counter = () => {
   const [counter, setCounter] = useState(10);
   function add(){
     setCounter(counter+1)
   }
 
+  return ( 
+    <div className="flex justify-center items-center flex-col">
+    <div className=" bg-slate-400 h-5 w-10 flex justify-center items-center rounded-md">
+      {counter}
+    </div>
+    <button onClick={add} className=" bg-slate-300 m-5 w-20 rounded-lg">add</button>
+  </div>
+   );
+}
+
+function App() {
+
   return (
     <div className="bg-black h-[100vh] flex justify-center items-center flex-col">
-      <div className=" bg-slate-400 h-5 w-10 flex justify-center items-center rounded-md">
-        {counter}
-      </div>
-      <button onClick={add} className=" bg-slate-300 m-5 w-20 rounded-lg">add</button>
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+
     </div>
   );
 }
+
+ 
+
 
 export default App;
