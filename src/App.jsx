a0a904850e62a2565e02654ac24917c0654047bb
frank <gamalgamer10@yahoo.com>
2023-05-05 16:11:11 +0300

    made a counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(10);
+  function add(){
+    setCounter(counter+1)
+  }
 
   return (
-    <div className="bg-black h-[100vh]"></div>
-  )
+    <div className="bg-black h-[100vh] flex justify-center items-center flex-col">
+      <div className=" bg-slate-400 h-5 w-10 flex justify-center items-center rounded-md">
+        {counter}
+      </div>
+      <button onClick={add} className=" bg-slate-300 m-5 w-20 rounded-lg">add</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
